ab55b223cd9444bc9a24ac957137a105dc01508c
Neketa <nekit53@gmail.com>
2021-10-11 13:32:52 +0300

    fix

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ function daysLeftUntilZelenyaComes() {
     var now = new Date();
     var comeback = new Date(2017, 6, 7, 0);
     var diff = comeback.getTime() - now.getTime();
-    var days = diff / 1000 / 60 / 60 / 24;
+    var days = Math.floor(diff / 1000 / 60 / 60 / 24);
+    var years = Math.floor(days / 365);
     var hours = 24 - now.getHours();
     var minutes = 60 -  now.getMinutes();
     var seconds = 60 - now.getSeconds();
-    document.getElementById("timer").innerHTML = Math.floor(days) + ' ' + 'days' + ' ' + hours  + ' ' + 'h' + ' ' + minutes + ' '  + 'min' + ' '  + seconds + ' '  + 'sec'; 
+    document.getElementById("timer").innerHTML = Math.abs(years) + ' ' + Math.abs(days) + ' ' + 'days' + ' ' + hours  + ' ' + 'h'; 
     setTimeout(daysLeftUntilZelenyaComes, 1000);
 };  
 daysLeftUntilZelenyaComes();
